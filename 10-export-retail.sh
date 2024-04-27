9f6e298c13e9a8ec2451bbeeb9a734a0d6b78deb
cloud-jake <jake.holmquist@kinandcarta.com>
2024-04-27 07:34:31 -0400

    Adding Analytics Metrics export

--- 10-export-retail.sh
+++ 10-export-retail.sh
@@ -17,8 +17,7 @@ PROJECT_NUM=`gcloud projects describe $PROJECT_ID --format="value(projectNumber)
 bq mk --project_id ${PROJECT_ID} --dataset ${EXPORT_DATASET} 
 
 
-
-# Create request.json
+# Create request.json for Product and User Event Data
 
 echo '{
   "outputConfig":
@@ -47,3 +46,25 @@ curl -X POST \
     -d @request.json \
     "https://retail.googleapis.com/v2alpha/projects/${PROJECT_ID}/locations/global/catalogs/default_catalog/branches/0/products:export"
 
+
+# Create request-analytics.json for Analytics Metrics Data
+
+echo '{
+  "catalog": "projects/'${PROJECT_ID}'/locations/global/catalogs/default_catalog",  
+  "outputConfig":
+  {
+    "bigqueryDestination":
+    {
+      "datasetId": "'${EXPORT_DATASET}'",
+        "tableIdPrefix": "'${EXPORT_PREFIX}'",
+        "tableType": "view"
+    }
+  }
+}' > request-analytics.json
+
+curl -X POST \
+     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+     -H "Content-Type: application/json; charset=utf-8" \
+     -H "x-goog-user-project: ${PROJECT_ID}" \ 
+     -d @request-analytics.json \
+     "https://retail.googleapis.com/v2/projects/${PROJECT_ID}/locations/global/catalogs/default_catalog:exportAnalyticsMetrics"
